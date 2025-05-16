c0fe73f2bd99d0f46cf3390564ce5eebda99ad65
Xavier Ritch <xavierritch@gmail.com>
2025-05-16 01:37:59 +0100

    fix: update the route

--- src/server.ts
+++ src/server.ts
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 8080;
 app.use(cors());
 app.use(express.json());
 
-app.use('/api/', routes);
+app.use('/', routes);
 
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
